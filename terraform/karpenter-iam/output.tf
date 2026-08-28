655db78efdb4a5cafc0f656e2b90be152f7a21ea
Paulo Junior <paulo.junior@wevy.cloud>
2026-08-28 09:04:11 -0300

    add: ESO

--- terraform/karpenter-iam/output.tf
+++
@@ -1,3 +0,0 @@
-output "role_arn" {
-  value = module.karpenter.role_arn
-}
--- terraform/karpenter-iam/variables.tf
+++
@@ -1,7 +0,0 @@
-
-variable "aws_region" {
-  default = "us-east-1"
-}
-variable "cluster_name" {
-  default = "platform-lab"
-}
